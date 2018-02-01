4f6dc742fe7e0408f564a6a35e8d0c28f87ce10f
aszamot <t.antkowiak@itgenerator.pl>
2018-02-01 17:55:16 +0100

    init commit

---
+++ SecondARProject/Assets/Scripts/BallScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    public GameObject Plane;
+    public GameObject SpawnPoint;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if( transform.position.y < Plane.transform.position.y - 100)
+        {
+            transform.position = SpawnPoint.transform.position;
+        }
+	}
+}
---
+++ SecondARProject/Assets/Scripts/EndZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndZone : MonoBehaviour {
+
+    public Text WinLabel;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "sphere")
+            WinLabel.text = "YAY!";
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "sphere")
+            WinLabel.text = "";
+    }
+}
